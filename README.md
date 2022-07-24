13119190e7e672da6ba9902b5238495cb0c5a42c
Io <wmt.tkn.121@gmail.com>
2022-07-24 23:30:48 +0900

    completed development and added README

---
+++ README.md
@@ -0,0 +1,92 @@
+# Rust RPN (Reverse Polish Notation) CLI
+
+This tool allows you to calculate the formula of Reverse Polish Notation written in standard input or in a text file.
+
+# DEMO
+
+# Features
+
+If you install Cargo, Rust's build system and package manager environment, you can perform the calculation by simply typing the `rpncalc` command.
+
+In addition, there is an optional feature that display the calculation process.
+
+# Requirement
+
+[environment]
+
+- Windows 10
+- cargo = "1.60.0"
+
+[dependencies]
+
+- anyhow = "1.0.58"
+- clap = "3.2.14"
+
+# Installation
+
+You have to install Cargo from [The Rust community’s crate registry](https://crates.io/) or [rustup](https://www.rust-lang.org/tools/install).
+
+```bash
+git clone https://github.com/iorn121/rust_RPN_cli
+
+cargo add clap
+cargo add anyhow
+
+cd rust_RPN_cli
+cargo install --path .
+```
+
+# Usage
+
+**stdin**
+
+```bash
+rpncalc
+
+3 4 +
+# 7 will be output
+```
+
+**text file**
+
+```bash
+echo "4 2 -" > rpn.txt
+
+rpncalc rpn.txt
+# 2 will be output
+```
+
+**option**
+
+```bash
+rpncalc -v
+
+2 7 + 3 - 7 * 3 /
+# ["/", "3", "*", "7", "-", "3", "+", "7"] [2]
+# ["/", "3", "*", "7", "-", "3", "+"] [2, 7]
+# ["/", "3", "*", "7", "-", "3"] [9]
+# ["/", "3", "*", "7", "-"] [9, 3]
+# ["/", "3", "*", "7"] [6]
+# ["/", "3", "*"] [6, 7]
+# ["/", "3"] [42]
+# ["/"] [42, 3]
+# [] [14]
+# 14
+# The above will be output
+```
+
+# Note
+
+This tool was created in Windows environment, so I have not verified that this will work on Mac or Linux.
+
+# Author
+
+- iorn121(Io)
+- github: @iorn121
+- twitter: @121Tkn
+- email: wmt.tkn.121@gmail.com
+- portfolio: https://iorn121.github.io/
+
+# License
+
+This tool is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,52 @@
 // use std::env;
+use anyhow::{bail,ensure,Context,Result};
 use clap::{App,Arg};
-fn main() {
+use std::fs::File;
+use std::io::{stdin,BufRead,BufReader};
+use std::path::PathBuf;
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+    pub fn eval(&self,formula: &str) -> Result<i32> {
+        let mut tokens=formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+    fn eval_inner(&self,tokens: &mut Vec<&str>) -> Result<i32> {
+        let mut stack=Vec::new();
+        let mut pos=0;
+
+        while let Some(token) = tokens.pop() {
+            pos+=1;
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().context(format!("invalid syntax at {}",pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}",pos))?;
+                let res = match token {
+                    "+" => x+y,
+                    "-" => x-y,
+                    "*" => x*y,
+                    "/" => x/y,
+                    "%" => x%y,
+                    _ => bail!("invalid troken at {}",pos),
+                };
+                stack.push(res);
+            }
+            if self.0 {
+                println!("{:?} {:?}",tokens,stack);
+            }
+        }
+
+        ensure!(stack.len()==1,"invalid syntax");
+
+        Ok(stack[0])
+    }
+}
+fn main() -> Result<()> {
     // let args: Vec<String> = env::args().collect();
     // println!("{:?}",args);
     let matches=App::new("My RPN program")
@@ -21,10 +67,56 @@ fn main() {
         )
         .get_matches();
     
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified")
-    }
     let verbose = matches.is_present("verbose");
-    println!("Verbose is {}?", verbose);
+    match matches.value_of("formula_file") {
+        Some(file) => {
+            let path = PathBuf::from(file);
+            let f=File::open(path)?;
+            let reader=BufReader::new(f);
+            run(reader, verbose)
+        },
+        _=>{
+        let stdin = stdin();
+        let reader=stdin.lock();
+        run(reader, verbose)
+        }
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
+    let calc=RpnCalculator::new(verbose);
+    for line in reader.lines() {
+        let line = line?;
+        match calc.eval(&line) {
+            Ok(answer)=>println!("{}", answer),
+            Err(e)=>println!("{:#?}", e)
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let calc=RpnCalculator::new(false);
+        assert_eq!(calc.eval("5").unwrap(),5);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
+
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("3 4 *").unwrap(), 12);
+        assert_eq!(calc.eval("4 5 -").unwrap(),-1);
+        assert_eq!(calc.eval("6 3 /").unwrap(),2);
+        assert_eq!(calc.eval("9 4 %").unwrap(),1);
+    }
+
+    #[test]
+    fn test_ng() {
+        let calc=RpnCalculator::new(false);
+        assert!(calc.eval("11*").is_err());
+        assert!(calc.eval("1 1 1 +").is_err());
+        assert!(calc.eval("+ 1 1").is_err());
+    }
 }
